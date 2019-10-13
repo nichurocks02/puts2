d1603b387381194ef5f0ebb9c7a5b79ceb5f5ca7
Nishkal <nichurocks02@gmail.com>
2019-10-13 15:14:19 +0200

    added multiplication functionality

--- main.py
+++ main.py
@@ -44,6 +44,23 @@ def sub():
   
 
 
+@app.route('/mul', methods=['GET','POST'])
+def mul():
+    value1=request.args.get('A',default = 0, type = Fraction)
+    value2=request.args.get('B',default = 0, type = Fraction)
+    C=value1*value2
+    D=str(C).split('/')
+    if len(D) == 2:
+        E=float(D[0])/float(D[1])
+        F=str(E).split(".")
+        if F[1] == '0':
+            return "%s\n" % F[0]
+        else:
+            return  "%s\n" %E
+    else:
+        G=str(C).split(".") 
+        return "%s \n" % G[0]   
+  
 
 
 if __name__ == "__main__":
