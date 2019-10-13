bebfe081117ff897ee7433c5bb2187af64aa7857
Nishkal <nichurocks02@gmail.com>
2019-10-13 14:05:54 +0200

    added first version of sub functionality to main.py to handle fractions

--- main.py
+++ main.py
@@ -8,23 +8,24 @@ def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/add')
-def addition():
+@app.route('/sub', methods=['GET','POST'])
+def sub():
     value1=request.args.get('A',default = 0, type = Fraction)
     value2=request.args.get('B',default = 0, type = Fraction)
-    C=value1+value2
+    C=value1-value2
     D=str(C).split('/')
     if len(D) == 2:
-    	E=float(D[0])/float(D[1])
-    	F=str(E).split(".")
-    	if F[1] == '0':
-    		return " %s\n" % F[0]
-    	else:
-    		return  " %s\n" %E
+        E=float(D[0])/float(D[1])
+        F=str(E).split(".")
+        if F[1] == '0':
+            return " %s\n" % F[0]
+        else:
+            return  " %s\n" %E
     else:
-    	G=str(C).split(".")	
-    	return " %s \n" % G[0]	
+        G=str(C).split(".") 
+        return "%s \n" % G[0]   
   
+
 
 
 
