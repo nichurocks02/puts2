872b765c36287ac3d1eaae63bba59433e4ec51b6
Nishkal <nichurocks02@gmail.com>
2019-10-13 13:59:42 +0200

    added 1st version of add function which can handle fractions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from fractions import Fraction 
 
 app = Flask(__name__)
 
@@ -9,11 +10,23 @@ def index():
 
 @app.route('/add')
 def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+    value1=request.args.get('A',default = 0, type = Fraction)
+    value2=request.args.get('B',default = 0, type = Fraction)
+    C=value1+value2
+    D=str(C).split('/')
+    if len(D) == 2:
+    	E=float(D[0])/float(D[1])
+    	F=str(E).split(".")
+    	if F[1] == '0':
+    		return " %s\n" % F[0]
+    	else:
+    		return  " %s\n" %E
+    else:
+    	G=str(C).split(".")	
+    	return " %s \n" % G[0]	
+  
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
