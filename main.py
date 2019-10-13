64651c39845ca8a0fcfaccbf1c042d456f1f3af5
Nishkal <nichurocks02@gmail.com>
2019-10-13 14:11:24 +0200

    second version of subfunctionality to main.py

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
+def addition():
+    value1=request.args.get('A',default = 0, type = Fraction)
+    value2=request.args.get('B',default = 0, type = Fraction)
+    C=value1+value2
+    D=str(C).split('/')
+    if len(D) == 2:
+        E=float(D[0])/float(D[1])
+        F=str(E).split(".")
+        if F[1] == '0':
+            return " %s\n" % F[0]
+        else:
+            return  " %s\n" %E
+    else:
+        G=str(C).split(".") 
+        return " %s \n" % G[0]  
+  
 
 @app.route('/sub', methods=['GET','POST'])
 def sub():
