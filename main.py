2327160dfea307679f9a3f116d2a05c85b0a67b3
Nishkal Gupta <43341611+nichurocks02@users.noreply.github.com>
2020-08-09 01:32:59 +0530

    added mean, median, max and min

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from fractions import Fraction 
+import statistics 
 
 app = Flask(__name__)
 
@@ -76,7 +77,192 @@ def div():
     else:
         G=str(C).split(".") 
         return " %s \n" % G[0]  
-  
-  
+
+
+@app.route('/average', methods=['GET','POST'])
+def average():
+    value1=request.args.get('X' )
+    sum=0
+
+    count=0
+    a=value1.split(',')
+    for i in a:
+        print(i)
+
+        match = re.search('/',i)
+        if match:
+            x=[]
+            n=i.split('/')
+            print(n)
+            for j in n :
+                print(j)
+                if j!='/':
+                    j= int(j)
+                    x.append(j)
+            div=x[0]/x[1]
+            sum=sum+div
+            count = count +1
+        else:
+            i=float(i)
+            sum=sum+i
+            count=count+1
+    avg = sum/count
+    D=str(avg).split('.')
+    if len(D) == 2:
+        if D[1]!=0:
+            return " %s\n" % avg
+        elif D[1]==0:
+            return " %s\n" % D[0]
+
+@app.route('/avg', methods=['GET','POST'])
+def avg():
+    value1=request.args.get('X' )
+    sum=0
+
+    count=0
+    a=value1.split(',')
+    for i in a:
+        print(i)
+
+        match = re.search('/',i)
+        if match:
+            x=[]
+            n=i.split('/')
+            print(n)
+            for j in n :
+                print(j)
+                if j!='/':
+                    j= int(j)
+                    x.append(j)
+            div=x[0]/x[1]
+            sum=sum+div
+            count = count +1
+        else:
+            i=float(i)
+            sum=sum+i
+            count=count+1
+    avg = sum/count
+    D=str(avg).split('.')
+    if len(D) == 2:
+        if D[1]!=0:
+            return " %s\n" % avg
+        elif D[1]==0:
+            return " %s\n" % D[0]
+
+@app.route('/mean', methods=['GET','POST'])
+def mean():
+        value1=request.args.get('X' )
+        sum=0
+
+        count=0
+        a=value1.split(',')
+        for i in a:
+            print(i)
+
+            match = re.search('/',i)
+            if match:
+                x=[]
+                n=i.split('/')
+                print(n)
+                for j in n :
+                    print(j)
+                    if j!='/':
+                        j= int(j)
+                        x.append(j)
+                div=x[0]/x[1]
+                sum=sum+div
+                count = count +1
+            else:
+                i=float(i)
+                sum=sum+i
+                count=count+1
+        avg = sum/count
+        D=str(avg).split('.')
+        if len(D) == 2:
+            if D[1]!=0:
+                return " %s\n" % avg
+            elif D[1]==0:
+                return " %s\n" % D[0]
+
+@app.route('/max', methods=['GET','POST'])
+def max():
+    value1=request.args.get('X' , type = str)
+    a=value1.split(',')
+    print(a)
+    y=[]
+    for i in a:
+        match = re.search('/',i)
+        if i!=',' :
+            if not match:
+                y.append(float(i))
+
+
+
+            if match:
+                x=[]
+                n=i.split('/')
+                for j in n:
+                    if j!='/':
+                        j= int(j)
+                        x.append(j)
+                div=x[0]/x[1]
+                y.append(div)
+
+    y.sort()
+    return " %s\n" % y[-1]
+
+@app.route('/min', methods=['GET','POST'])
+def min():
+    value1=request.args.get('X' , type = str)
+    a=value1.split(',')
+    print(a)
+    y=[]
+    for i in a:
+        match = re.search('/',i)
+        if i!=',' :
+            if not match:
+                y.append(float(i))
+
+
+
+            if match:
+                x=[]
+                n=i.split('/')
+                for j in n:
+                    if j!='/':
+                        j= int(j)
+                        x.append(j)
+                div=x[0]/x[1]
+                y.append(div)
+
+    y.sort()
+    return " %s\n" % y[0]
+
+@app.route('/median', methods=['GET','POST'])
+def median():
+    value1=request.args.get('X' , type = str)
+    a=value1.split(',')
+    print(a)
+    y=[]
+    for i in a:
+        match = re.search('/',i)
+        if i!=',' :
+            if not match:
+                y.append(float(i))
+
+            if match:
+                x=[]
+                n=i.split('/')
+                for j in n:
+                    if j!='/':
+                        j= int(j)
+                        x.append(j)
+                div=x[0]/x[1]
+                y.append(div)
+    med=statistics.median(y)
+
+    return " %s\n" % med
+
+
 if __name__ == "__main__":
     app.run(debug=True)
